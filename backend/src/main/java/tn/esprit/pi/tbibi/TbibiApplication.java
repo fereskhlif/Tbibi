bdadb6cdb738ba9ee2d34815bc8074600aabdba1
Feres Khlif <fereskhlif@gmail.com>
2026-02-11 19:00:38 +0100

    creation du projet

---
+++ backend/src/main/java/tn/esprit/pi/tbibi/TbibiApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.pi.tbibi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbibiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TbibiApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/tn/esprit/pi/tbibi/TbibiApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.pi.tbibi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbibiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
