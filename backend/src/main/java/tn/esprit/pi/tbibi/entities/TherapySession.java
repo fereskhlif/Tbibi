6e3a2f809a99da7c3395e51aca5b3e3f1801bf27
Feres Khlif <fereskhlif@gmail.com>
2026-02-14 22:05:40 +0100

    ajout des corrections

--- backend/src/main/java/tn/esprit/pi/tbibi/entities/TherapySession.java
+++ backend/src/main/java/tn/esprit/pi/tbibi/entities/TherapySession.java
@@ -1,11 +1,8 @@
 package tn.esprit.pi.tbibi.entities;
-
 import jakarta.persistence.*;
 import lombok.*;
-
 import java.time.LocalDate;
 import java.time.LocalTime;
-
 @Entity
 @Table(name = "therapy_sessions")
 @Getter
@@ -14,27 +11,18 @@ import java.time.LocalTime;
 @AllArgsConstructor
 @ToString
 public class TherapySession {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer sessionId;
-
     private String progressNote;
-
     private LocalDate scheduledDate;
-
     private String evaluationResult;
-
     private LocalTime startTime;
-
     private LocalTime endTime;
-
     @ManyToOne
     @JoinColumn(name = "patient_user_id", nullable = false)
-
     private User patient;
     @ManyToOne
     @JoinColumn(name = "physiotherapist_user_id", nullable = false)
-
     private User physiotherapist;
 }
