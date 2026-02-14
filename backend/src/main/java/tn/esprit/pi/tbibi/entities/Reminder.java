877af98d94ce2026b059c81786e0ece4cd65d164
bedhiafiranim <bedhiafiranim0@gamil.com>
2026-02-14 23:03:40 +0100

    Add Reminder and MonitoringOfChronicDisease entities

---
+++ backend/src/main/java/tn/esprit/pi/tbibi/entities/Reminder.java
@@ -0,0 +1,21 @@
+package tn.esprit.pi.tbibi.entities;
+import java.time.LocalDate;
+import jakarta.persistence.*;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Reminder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reminderId;
+
+    private LocalDate heureRappel;
+
+    private Integer frequence;
+}
