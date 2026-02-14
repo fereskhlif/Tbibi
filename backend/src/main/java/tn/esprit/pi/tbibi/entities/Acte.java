8393a647d83df158958eb713145fca65dbb6f7f6
Feres Khlif <fereskhlif@gmail.com>
2026-02-14 17:19:07 +0100

    ajout des corrections

--- backend/src/main/java/tn/esprit/pi/tbibi/entities/Acte.java
+++ backend/src/main/java/tn/esprit/pi/tbibi/entities/Acte.java
@@ -1,20 +1,16 @@
 package tn.esprit.pi.tbibi.entities;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
-
 import java.util.Date;
-
 @AllArgsConstructor
 @Entity
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
-
 public class Acte {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- backend/src/main/java/tn/esprit/pi/tbibi/entities/Role.java
+++ backend/src/main/java/tn/esprit/pi/tbibi/entities/Role.java
@@ -1,16 +1,27 @@
 package tn.esprit.pi.tbibi.entities;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.List;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
 public class Role {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int role_id;
+
     private String name_patient;
     private String kine_name;
     private String docteur_name;
     private String pharmasis;
     private String laboratory_group;
+
+    @OneToMany(mappedBy = "role")
+    private List<User> users;
 }
