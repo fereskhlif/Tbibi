2a3b5469c7dbd4318f3f08db8ab4d61e853ae954
Yassine Shili <yassine1.shili@outlook.fr>
2026-02-14 20:26:44 +0100

    Merge

---
+++ backend/src/main/java/tn/esprit/pi/tbibi/entities/Acte.java
@@ -0,0 +1,22 @@
+package tn.esprit.pi.tbibi.entities;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import java.util.Date;
+@AllArgsConstructor
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Acte {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int acte_id;
+    private Date date;
+    private String description;
+    private String typeOfActe;
+
+}
