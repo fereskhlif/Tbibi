60bd53d31ec8d191d67ef29594774e4d0727ac4d
Feres Khlif <fereskhlif@gmail.com>
2026-02-14 16:52:57 +0100

    ajout les classes user et role

--- backend/src/main/java/tn/esprit/pi/tbibi/entities/User.java
+++ backend/src/main/java/tn/esprit/pi/tbibi/entities/User.java
@@ -1,25 +1,32 @@
 package tn.esprit.pi.tbibi.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
+import java.util.List;
 
-@AllArgsConstructor
 @Entity
+@Table(name = "users")
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
-
+@AllArgsConstructor
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int user_id;
+    private int userId;
+
     private String name;
-    private String Email;
     private String email;
     private String password;
     private String adresse;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "patient_id")
+    private List<MedicalReccords> medicalFiles;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "role_id")
+    private Role role;
 }
