73382517f7f916dc8979fc3fbb21f98969fb9bd3
Feres Khlif <fereskhlif@gmail.com>
2026-02-23 00:06:13 +0100

    crud prescription

--- backend/src/main/java/tn/esprit/pi/tbibi/services/MedRec_Mapper.java
+++ backend/src/main/java/tn/esprit/pi/tbibi/services/MedRec_Mapper.java
@@ -7,6 +7,6 @@ import tn.esprit.pi.tbibi.entities.MedicalReccords;
 
 @Mapper(componentModel = "spring")
 public interface MedRec_Mapper {
-    MedicalReccords toEntity(MdicalReccordsRequest request);       // Request  → Entity
-    MdicalReccordsResponse toResponse(MedicalReccords entity);     // Entity   → Response ✅
+    MedicalReccords toEntity(MdicalReccordsRequest request);
+    MdicalReccordsResponse toResponse(MedicalReccords entity);
 }
