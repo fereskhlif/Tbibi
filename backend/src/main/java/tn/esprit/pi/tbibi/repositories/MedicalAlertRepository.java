2b2c2a5012392b622709e2da9114b35b64b35de7
bedhiafiranim <bedhiafiranim0@gamil.com>
2026-02-24 00:55:51 +0100

    Remove MedicalAlert CRUD

--- backend/src/main/java/tn/esprit/pi/tbibi/repositories/MedicalAlertRepository.java
+++
@@ -1,8 +0,0 @@
-package tn.esprit.pi.tbibi.repositories;
-import org.springframework.data.jpa.repository.JpaRepository;
-import tn.esprit.pi.tbibi.entities.MedicalAlert;
-
-public interface MedicalAlertRepository extends JpaRepository<MedicalAlert, Long> {
-
-}
-
