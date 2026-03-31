937cc7efb7f415583d607b700cdc7741e26cf070
MD-lemin <medlemin418@gmail.com>
2026-03-31 09:17:14 +0200

    Fix UserController API path: add /api prefix to match other controllers

--- backend/src/main/java/tn/esprit/pi/tbibi/controllers/UserController.java
+++ backend/src/main/java/tn/esprit/pi/tbibi/controllers/UserController.java
@@ -23,7 +23,7 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
-@RequestMapping({"/users", "/user"})
+@RequestMapping({"/api/users", "/api/user"})
 @RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
 public class UserController {
